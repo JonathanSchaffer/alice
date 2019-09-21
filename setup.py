5321dd73a49a44b4611f383e4b47d6029bbe3186
JonathanSchaffer <55607246+JonathanSchaffer@users.noreply.github.com>
2019-09-21 07:09:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,4 @@
 f = open("settings.conf", "w")
 f.write(raw_input("please enter a path to the folder containing music: "))
+system("sudo apt-get install libttspico-utils")
+system("sudo apt-get install timidity")
