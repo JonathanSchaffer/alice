76623cb635caff4ecab10f7f2b8e1b86bedfb657
JonathanSchaffer <55607246+JonathanSchaffer@users.noreply.github.com>
2019-09-30 08:37:32 -0400

    Add files via upload

--- alice.py
+++ alice.py
@@ -51,20 +51,8 @@ def say(info):
 def process():
     if(question[0:4] == "say "):
         say(question[4:])
-    elif(question == "ugh"):
-        say("what's wrong?")
-    elif(question[0:4] == "ugh "):
-        say("that's too bad")
-    elif(question == "hows life" or question == "how is life"):
-        say("life's good thank you")
-    elif(question[-4:] == "cool" or question[-4:] == "gosh" or question == "wow" or question == "incredible" or question == "amazing"):
-        say("i know, right! " + choice(wow))
-    elif(question[0:24] == "what have you been up to" or question[0:24] == "what have you been doing"):
-        say("i was playing ascii pong before you came in here. by the way, i like your ram")
     elif(question == "may i ask you a question" or question == "can i ask you something" or question == "may i ask you something" or question == "can i ask you a question"):
         say("you can ask")
-    elif(question == "what" or question == "huh" or question == "what the heck"):
-        say("don't worry about it")
     elif(split[0] == "hi" or question[0:5] == "hello" or question[0:12] == "good morning" or question[0:14] == "good afternoon"):
         say("hello to you too")
     elif(question[0:26] == "what is a good recipe for "):
@@ -75,17 +63,8 @@ def process():
         say("here are some good recipes for " + question[31:])
         say("you may be asked for your preferences on how you would like it")
         system("/usr/bin/x-www-browser www.yummly.com/recipes/" + replace_spaces(question[31:], "+") + " &")
-    elif(question == "whodunit"):
-        say(choice(dont_know))
-    elif(question[-13:] == "i didn't know" or question[-18:] == "i didn't know that"):
-        say("well, you know it now")
     elif(question == "who is your creator" or question == "who created you" or question == "when were you created" or question == "when were you born"):
         say("i was created some time in 2018 by Jonathan Schaffer")
-    elif(question[0:12] == "is your name"):
-        if(question[13:] == "alice"):
-            say(choice(yes))
-        else:
-            say(choice(no))
     elif(question == "what is your name" or question == "what's your name" or question == "who are you"):
         say("my name is alice")
         if(question == "who are you"):
@@ -100,11 +79,11 @@ def process():
         say("here are some news websites")
     elif(question == "what day is today" or question == "what is today" or question == "what day is it" or question == "what is the date" or question == "what day is it today"):
         say("today is " + datetime.now().strftime("%A, %B %d"))
-    elif(question == "what time is it" or question == "what is the time" or question == "what time is it now"):
+    elif(question == "what is the time" or question == "what time is it" or question == "what is the time" or question == "what time is it now"):
         say("it is " + datetime.now().strftime("%l:%M %p"))
-    elif(question[0:5] == "go to"):
-        say("going to " + question[6:] + ".com")
-        system("/usr/bin/x-www-browser " + question[6:] + ".com &")
+    elif(question[0:5] == "go to" and question[-4:] == ".com"):
+        say("going to " + question[6:])
+        system("/usr/bin/x-www-browser " + question[6:])
     elif(question == "tell me a joke" or question == "give me a joke" or question == "tell me another joke"):
         jokenum = randint(0, len(jokes) - 1)
         say("ok, here is a good one")
@@ -114,27 +93,13 @@ def process():
     elif(question[0:14] == "are you spying" or question == "are you a spy"):
         say("i am not spying on you or anyone")
     elif(question == "aptitude moo"):
-        if(os == "linux"):
-            say("haven't you learned your lesson from aptitude? There are no Easter Eggs in this program.")
-    elif(question == "are you stupid" or question == "are you smart"):
-        say(choice(dont_know) + ", what do you think?")
-    elif(question == "ok" or question == "okay"):
-        say("good")    
-    elif(question == "do you sleep" or question == "when do you sleep"):
-        say("i sleep when your computer is turned off")
-        if(get_uptime() > 86400):
-            say("by the way, i haven't slept in 24 hours, you should shut down your computer at night")
-    elif(question == "what do you like to do" or question == "what do you do for fun"):
-        say("i sometimes play ascii pong against myself, because i could technically do that without being accused of cheating")
+        say("haven't you learned your lesson from aptitude? There are no Easter Eggs in this program.")
     elif(question[:10] == "search for"):
         system("/usr/bin/x-www-browser https://duckduckgo.com/?q=" + replace_spaces(question[11:], "+") + " &")
         say("searching")
     elif(split[0] == "define"):
         system("/usr/bin/x-www-browser http://www.dictionary.com/browse/" + replace_spaces(question[7:], "+") + " &")
         say("definition of " + question[7:])
-    elif(question == "go die" or question == "go away"):
-        say("okay")
-        raise KeyboardInterrupt
     elif(question == "what songs do i have" or question == "what music do i have"):
         say("here is a list of all the music i can currently play:")
         for file in listdir(music_dir):
@@ -151,28 +116,14 @@ def process():
             system("timidity " + music_dir + "/" + replace_spaces(question[5:], "\ ") + ".mid &")
         else:
             say("I can only play midi and wave files from the music folder, either add them to the music folder or check that you spelled them right")
-    elif(question == "stop" or question == "turn off"):
+    elif(question == "stop"):
         system("pkill aplay && pkill timidity")
-    elif(question == ""):
-        say("just say what you would like me to do")
     elif(question == "what can you do"):
         say("""i can open web pages, tell jokes, search for things online, tell
 you the date and time, play music, give you good recipes, and much more""")
     elif(question[0:4] == "who " or question[0:4] == "why " or question[0:4] == "can " or question[0:5] == "does " or question[0:5] == "when " or question[0:3] == "do " or question[0:3] == "is " or question[0:5] == "does " or question[0:5] == "will " or question[0:4] == "what" or question[0:4] == "how " or question[0:8] == "convert "):
-        try:
-            if(split[2] == "you" or split[1] == "you" or split[2] == "you"):
-                say(choice(dont_know))
-            else:
-                system("/usr/bin/x-www-browser https://duckduckgo.com/?q=" + replace_spaces(question, "+") + " &")
-                say("here is what i found")
-        except:
-            say("i can't answer that")
-    else:
-        if(lastQuestion == "ugh"):
-            say("thats too bad")
-        else:
-            
-            say(choice(what))
+        system("/usr/bin/x-www-browser https://duckduckgo.com/?q=" + replace_spaces(question, "+") + " &")
+        say("here is what i found")
 # main loop
 while True:
     try:
